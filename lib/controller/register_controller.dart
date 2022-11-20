e64a812e9e18a786b7131c78d14c588d9ec1b12b
AfshinNajafi <afshinnajafi74@gmail.com>
2022-11-20 11:53:25 +0300

    route to write article and podcast bottomsheet in registerController

--- lib/controller/register_controller.dart
+++ lib/controller/register_controller.dart
@@ -1,5 +1,6 @@
 import 'dart:developer';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:tec/component/api_constant.dart';
 import 'package:tec/component/storage_const.dart';
@@ -7,6 +8,8 @@ import 'package:tec/services/dio_service.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:tec/view/main_screen/main_screen.dart';
 import 'package:tec/view/register/register_intro.dart';
+
+import '../gen/assets.gen.dart';
 
 class RegisterController extends GetxController{
   TextEditingController emailTextEditingController = TextEditingController();
@@ -59,7 +62,70 @@ class RegisterController extends GetxController{
     if(GetStorage().read(token) == null){
       Get.to(RegisterIntro());
     }else{
-      debugPrint("post screen");
+      routeToWriteBottomSheet();
     }
+  }
+
+  routeToWriteBottomSheet(){
+    Get.bottomSheet(
+      Container(
+        height: Get.height / 3,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          )
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  SvgPicture.asset(Assets.images.tcbot,height: 40,),
+                  const SizedBox(width: 8,),
+                  Text("دونسته هات رو با بقیه به اشتراک بذار ...")
+                ],
+              ),
+              SizedBox(height: 8,),
+              Text("""
+فکر کن !!  اینجا بودنت به این معناست که یک گیک تکنولوژی هستی
+دونسته هات رو با  جامعه‌ی گیک های فارسی زبان به اشتراک بذار.."""),
+              const SizedBox(height: 28,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      debugPrint("write article");
+                    },
+                    child: Row(
+                      children: [
+                        Image.asset("assets/icons/write_article_icon.png",height: 32,),
+                        const SizedBox(width: 8,),
+                        Text("مدیریت مقاله ها")
+                      ],
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      debugPrint("write podcast");
+                    },
+                    child: Row(
+                      children: [
+                        Image.asset("assets/icons/write_podcast_icon.png",height: 32,),
+                        const SizedBox(width: 8,),
+                        Text("مدیریت پادکست ها")
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
 }
